49e52aa77d3016dfa958a493e14c53511094344b
sidpixel <45841651+sidpixel@users.noreply.github.com>
2023-08-28 16:08:55 +0800

    add oaf插件仓库

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,10 +15,10 @@ rm -rf package/lean/luci-theme-argon
 # 拉取argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
-# 拉取微信推送、京东签到插件
+# 拉取微信推送插件
 git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
-#git clone https://github.com/jerrykuku/node-request.git package/lean/node-request
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
+# 拉取oaf流控插件
+git clone https://github.com/destan19/OpenAppFilter.git  package/OpenAppFilter
 
 #定义时间变量
 echo "FILE_DATE=$(date +%Y%m%d%H%M)" >>"$GITHUB_ENV"
