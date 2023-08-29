5bc2e01463e205df880ea70eb0414a016a4f9885
sidpixel <45841651+sidpixel@users.noreply.github.com>
2023-08-29 09:05:11 +0800

    modify author

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
-#============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+#=================================================
+# https://github.com/sidpixel/lede
+# Description: Build OpenWrt using GitHub Actions
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#============================================================
+# Author: sidpixel
+#=================================================
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
