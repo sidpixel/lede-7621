414bdbea929dd9be47d7057c4e8027c03f570e8d
sidpixel <45841651+sidpixel@users.noreply.github.com>
2023-09-07 15:43:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,9 +13,9 @@ sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # 拉取微信推送插件
-git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+#git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 # 拉取oaf流控插件
-git clone https://github.com/destan19/OpenAppFilter/tree/master/luci-app-oaf  package/luci-app-oaf
+#git clone https://github.com/destan19/OpenAppFilter/tree/master/luci-app-oaf  package/luci-app-oaf
 
 #定义时间变量
 echo "FILE_DATE=$(date +%Y%m%d%H%M)" >>"$GITHUB_ENV"
